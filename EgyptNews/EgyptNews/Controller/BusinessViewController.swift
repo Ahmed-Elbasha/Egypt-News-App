da043f374627d4d1e9c3111c9093aee41d095a49
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-22 18:39:32 +0200

    Modified The UI of The App

--- EgyptNews/EgyptNews/Controller/BusinessViewController.swift
+++ EgyptNews/EgyptNews/Controller/BusinessViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AlamoFire
+import AlamoFireImage
 
 class BusinessViewController: UIViewController {
 
--- EgyptNews/EgyptNews/Controller/SportsViewController.swift
+++ EgyptNews/EgyptNews/Controller/SportsViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class SportsViewController: UIViewController {
 
