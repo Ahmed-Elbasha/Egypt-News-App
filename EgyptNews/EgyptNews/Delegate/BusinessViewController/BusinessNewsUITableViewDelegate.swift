85a367043a7aae3997a89a8d9edfc0e31c0b9716
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-31 21:49:53 +0200

    Modified The Implementation and UI Design of BusinessViewController

--- EgyptNews/EgyptNews/Delegate/BusinessViewController/BusinessNewsUITableViewDelegate.swift
+++ EgyptNews/EgyptNews/Delegate/BusinessViewController/BusinessNewsUITableViewDelegate.swift
@@ -17,15 +17,28 @@ extension BusinessViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessNewsArticleCell", for: indexPath) as? BusinessNewsArticleCell else { return UITableViewCell() }
         let newsArticle = newsArticles[indexPath.row]
-        let currentImage = images[indexPath.row]
-        if cell.isKind(of: BusinessNewsArticleCell.self) {
-            cell.configureCell(withNewsArticle: newsArticle, andNewsArticleImage: currentImage)
-        } else if cell.isKind(of: UITableViewCell.self) {
-            cell.newsArticleLabel.text = newsArticle.articleTitle
-            cell.newsArticleSourceLabel.text = newsArticle.articleSourceName
-            cell.publishDateLabel.text = newsArticle.publishDate
-            cell.newsArticleImageView.image = currentImage
+        var currentImage = try images[indexPath.row] 
+        if currentImage != nil {
+            if cell.isKind(of: BusinessNewsArticleCell.self) {
+                cell.configureCell(withNewsArticle: newsArticle, andNewsArticleImage: currentImage)
+            } else if cell.isKind(of: UITableViewCell.self) {
+                cell.newsArticleLabel.text = newsArticle.articleTitle
+                cell.newsArticleSourceLabel.text = newsArticle.articleSourceName
+                cell.publishDateLabel.text = newsArticle.publishDate
+                cell.newsArticleImageView.image = currentImage
+            }
+        } else {
+            currentImage = UIImage(named: "no_image")!
+            if cell.isKind(of: BusinessNewsArticleCell.self) {
+                cell.configureCell(withNewsArticle: newsArticle, andNewsArticleImage: currentImage)
+            } else if cell.isKind(of: UITableViewCell.self) {
+                cell.newsArticleLabel.text = newsArticle.articleTitle
+                cell.newsArticleSourceLabel.text = newsArticle.articleSourceName
+                cell.publishDateLabel.text = newsArticle.publishDate
+                cell.newsArticleImageView.image = currentImage
+            }
         }
+        
         return cell
     }
     
